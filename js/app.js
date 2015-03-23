0f8b98cb83cd73e690906605ec7c8a1b1a12c3f3
Heather Billings <heatherjaybillings@gmail.com>
2015-03-23 10:29:07 -0500

    resize elements options

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+$(document).ready(function(){
+  // Change the size of the style guide div to mimic various device widths
+  $('#change-view.sub-nav dd').click(function(){
+    $('#style-guide').removeClass().addClass($(this).attr('class'));
+  });
+});
