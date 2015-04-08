563d1409ca847d5b4248b6040acdcba64e067867
Heather Billings <heatherjaybillings@gmail.com>
2015-04-08 15:50:52 -0500

    finish accoridon bugs

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
 $(document).ready(function(){
   // Change the size of the style guide div to mimic various device widths
   $('#change-view.sub-nav dd').click(function(){
     $('#style-guide').removeClass().addClass($(this).attr('class'));
+  });
+  $(".accordion").on("click", "li", function (event) {
+        event.preventDefault();
+        if($(this).hasClass('active')){
+            $("li.active").removeClass('active').find(".content").slideUp("fast");    
+        }
+        else {
+            $("li.active").removeClass('active').find(".content").slideUp("fast");
+            $(this).addClass('active').find(".content").slideToggle("fast");    
+        }
   });
 });
